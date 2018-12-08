bef3e786033e2c4e730f901e173a3f6b41047f3d
Aivan Monceller <aivan@monceller.com>
2018-12-08 19:42:08 +0800

    style(main): add header, element-ui, scss

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,28 +1,76 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-container id="app">
+    <el-header class="v-main-header"><Header/></el-header>
+    <el-main>
+      <div id="main">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+      </div>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Header
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+<style lang="scss">
+#main {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+html,
+body,
+#app.el-container,
+.el-menu-vertical {
+  height: 100%;
+}
+#app  > .el-container {
+  flex-direction: column;
+  .el-header {
+    box-sizing: content-box;
+  }
+  .el-menu {
+    border-right: 0;
+  }
+  .el-main .container {
+    min-height: calc(100vh - 110px);
+  }
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.v-main-header {
+  border-bottom: solid 1px #e6e6e6;
 }
 </style>
---
+++ webapp/src/components/Header.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <el-menu
+      :default-active="activeIndex"
+      class="v-menu-header"
+      mode="horizontal"
+      @select="handleSelect" >
+      <div class="v-menu-header-logo">
+        <el-menu-item index="0">
+          <img
+            class="logo-image"
+            src="../assets/logo.png"
+            alt="logo">
+          <span class="logo-name">Jobsite</span>
+        </el-menu-item>
+      </div>
+      <el-menu-item index="1">Items 1</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">Items 2</template>
+        <el-menu-item index="2-1">item one</el-menu-item>
+        <el-menu-item index="2-2">item two</el-menu-item>
+        <el-menu-item index="2-3">item three</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">item four</template>
+          <el-menu-item index="2-4-1">item one</el-menu-item>
+          <el-menu-item index="2-4-2">item two</el-menu-item>
+          <el-menu-item index="2-4-3">item three</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item
+        index="3"
+        disabled>Items 3</el-menu-item>
+      <el-menu-item index="4">
+        <a href="/">Items 4</a>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      activeIndex: '1'
+    }
+  },
+  methods: {
+    handleSelect (e) {
+      console.log('selected', e)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.v-menu-header-logo {
+  width: auto;
+  height: 100%;
+  float: left;
+  overflow: hidden;
+  padding-left: 20px;
+  .logo-image {
+    width: 40px;
+    height: 40px;
+  }
+  .logo-name {
+    font-size: 18px;
+  }
+  .el-menu-item {
+    display: inline-block;
+  }
+  .el-menu-item:hover {
+    background: transparent;
+  }
+}
+</style>
